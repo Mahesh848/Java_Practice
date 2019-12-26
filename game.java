4958efae2b271a767cabe88270ffcc18e86be7d7
Mahesh Ippili <ippilimahesh1999@gmail.com>
2019-12-26 21:59:16 +0530

    Pathfinding problem modified

--- game.java
+++ game.java
@@ -1,16 +1,24 @@
 class Grid {
     int grid[][] = new int[10][10];
-    int path[][] = new int[100][2];
-    int length = 99999999;
 
     void assignTheBallLocation(int x, int y) {
         this.grid[x][y] = 1;
+    }
+}
+
+class Person {
+    int grid[][];
+    int path[][] = new int[100][2];
+    int length = 99999999;
+
+    Person(Grid grid) {
+        this.grid = grid.grid;
     }
 
     void findPath(int x, int y, int path[][], int path_len) {
         path[path_len][0] = x;
         path[path_len][1] = y;
-        if (x < 10 && y < 10 && grid[x][y] == 1) {
+        if (x < 10 && y < 10 && this.grid[x][y] == 1) {
             if (path_len < length) {
                 length = path_len;
                 for (int i = 0; i < path_len; i++) {
@@ -29,28 +37,33 @@ class Grid {
         path[path_len][0] = -1;
         path[path_len][1] = -1;
     }
+}
 
-    int[][] getPath() {
-        return this.path;
-    }
+class Ball {
+    int x, y;
 
-    int getPathLength() {
-        return this.length;
+    void generateRandomCoordinates() {
+        this.x = (int) (Math.random() * 10);
+        this.y = (int) (Math.random() * 10);
     }
 }
 
-class Test {
+class Game {
     public static void main(String[] args) {
-        Grid g = new Grid();
-        int x = (int) (Math.random() * 10);
-        int y = (int) (Math.random() * 10);
-        g.assignTheBallLocation(x, y);
-        g.findPath(0, 0, new int[100][2], 0);
-        int[][] path = g.getPath();
-        int length = g.getPathLength();
+        Ball ball = new Ball();
+        ball.generateRandomCoordinates();
+
+        Grid grid = new Grid();
+        grid.assignTheBallLocation(ball.x, ball.y);
+
+        Person person = new Person(grid);
+        person.findPath(0, 0, new int[100][2], 0);
+
+        int[][] path = person.path;
+        int length = person.length;
         for (int i = 0; i < length; i++) {
             System.out.print(path[i][0] + "," + path[i][1] + "  ");
         }
-        System.out.println(x + "," + y);
+        System.out.println(ball.x + "," + ball.y);
     }
 }
