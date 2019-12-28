91f2f2bf0e9e2bd739c7ff1643fbdf5e7758db91
Mahesh Ippili <ippilimahesh1999@gmail.com>
2019-12-28 14:12:50 +0530

    Game.java modified according to feedback

--- game.java
+++ game.java
@@ -1,26 +1,22 @@
 class Grid {
     int grid[][] = new int[10][10];
+    Ball ball;
 
-    void assignTheBallLocation(int x, int y) {
-        this.grid[x][y] = 1;
+    void setTheBall(Ball ball) {
+        this.ball = ball;
     }
 }
 
 class Person {
-    int grid[][];
     int path[][] = new int[100][2];
-    int length = 99999999;
+    int path_length = 99999999;
 
-    Person(Grid grid) {
-        this.grid = grid.grid;
-    }
-
-    void findPath(int x, int y, int path[][], int path_len) {
+    void findPath(Grid grid, int x, int y, int path[][], int path_len) {
         path[path_len][0] = x;
         path[path_len][1] = y;
-        if (x < 10 && y < 10 && this.grid[x][y] == 1) {
-            if (path_len < length) {
-                length = path_len;
+        if (x < 10 && y < 10 && grid.ball.x == x && grid.ball.y == y) {
+            if (path_len < path_length) {
+                path_length = path_len;
                 for (int i = 0; i < path_len; i++) {
                     this.path[i][0] = path[i][0];
                     this.path[i][1] = path[i][1];
@@ -29,10 +25,10 @@ class Person {
             return;
         }
         if (x < 10) {
-            findPath(x + 1, y, path, path_len + 1);
+            findPath(grid, x + 1, y, path, path_len + 1);
         }
         if (y < 10) {
-            findPath(x, y + 1, path, path_len + 1);
+            findPath(grid, x, y + 1, path, path_len + 1);
         }
         path[path_len][0] = -1;
         path[path_len][1] = -1;
@@ -54,14 +50,14 @@ class Game {
         ball.generateRandomCoordinates();
 
         Grid grid = new Grid();
-        grid.assignTheBallLocation(ball.x, ball.y);
+        grid.setTheBall(ball);
 
-        Person person = new Person(grid);
-        person.findPath(0, 0, new int[100][2], 0);
+        Person person = new Person();
+        person.findPath(grid, 0, 0, new int[100][2], 0);
 
         int[][] path = person.path;
-        int length = person.length;
-        for (int i = 0; i < length; i++) {
+        int path_length = person.path_length;
+        for (int i = 0; i < path_length; i++) {
             System.out.print(path[i][0] + "," + path[i][1] + "  ");
         }
         System.out.println(ball.x + "," + ball.y);
